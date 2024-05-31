377f1a0ad8a9245968798e0437eee78c8b23fcc6
hibikiggg <hibi.jaianbb@icloud.com>
2024-05-31 21:22:49 +0900

    爆弾初期

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
-const bombs = (newUserInputs, setuserInputs) => {
-  const math = Math.floor(Math.random() * 9);
-  const newmath = Math.floor(Math.random() * 9);
-  console.log(math, newmath);
-  for (let m = 0; m < 10; m++) {
-    const math = Math.floor(Math.random() * 9);
-    const newmath = Math.floor(Math.random() * 9);
-    newUserInputs[math][newmath] = 11;
-    setuserInputs(newUserInputs);
+const directions = [
+  [1, 0],
+  [1, 1],
+  [0, 1],
+  [1, -1],
+  [-1, 0],
+  [-1, -1],
+  [0, -1],
+  [-1, 1],
+];
+const bombs = (newUserInputs: number[][]) => {
+  if (newUserInputs.flat().filter((cell) => cell === 0).length === 81) {
+    for (let m = 0; m < 10; m++) {
+      const randomY = Math.floor(Math.random() * 9);
+      const randomX = Math.floor(Math.random() * 9);
+      console.log(randomY, randomX);
+      newUserInputs[randomY][randomX] = 11;
+      // setuserInputs(newUserInputs);
+    }
   }
+  return newUserInputs;
 };
 const Home = () => {
   const [samplePos, setsamplePos] = useState(0);
@@ -26,15 +37,6 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const ClickHandler = (x: number, y: number) => {
-    // console.log(x, y);
-    const newUserInputs = structuredClone(userInputs);
-    bombs(newUserInputs, setuserInputs);
-    console.log(x,y);
-    for(a = 0;a < 81);
-
-  };
-
   const [userInputs, setuserInputs] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -46,8 +48,27 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const board: number[][] = [];
-  console.log(samplePos);
+
+  const ClickHandler = (x: number, y: number) => {
+    // console.log(x, y);
+
+    const newUserInputs = structuredClone(userInputs);
+    const bombset = bombs(newUserInputs);
+    console.log(bombset);
+    setuserInputs(bombset);
+    for (const [dx, dy] of directions) {
+      const a = 1;
+      while (a > 0 && a < 9) {
+        const ax = x + a * dx;
+        const bx = y + a * dy;
+        if (ax >= 8 || bx >= 8 || ax < 0 || bx < 0 || bombMap[ax][bx] === 0) {
+          break;
+        }
+      }
+    }
+  };
+  // const board: number[][] = [];
+  // console.log(samplePos);
 
   return (
     <div className={styles.container}>
